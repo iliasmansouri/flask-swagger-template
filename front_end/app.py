2f8c85ecbb7c6feec106b9014adf032ffc621e52
Ilias Mansouri <iliasmansouri@Iliass-MBP.lan>
2021-10-13 17:38:20 +0200

    fix relative imports

--- front_end/app.py
+++ front_end/app.py
@@ -8,3 +8,7 @@ def run():
     radio_value = st.sidebar.radio("Select task", tuple(t.value for t in TxType))
     st_response = post_request.post(radio_value)
     st_response.show()
+
+
+if __name__ == "__main__":
+    run()
